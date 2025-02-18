bfc5b8f471a0cd99cff6cd960e173f8cdd2aca2c
Muhammad Ali <rajaa4856@gmail.com>
2025-02-18 21:52:14 +0500

    uninstalled the filament and install agfain and fixed the admin login issue using admins login

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,48 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'email', 'password'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
+
+
+
+
+
+
+
+
+
+
